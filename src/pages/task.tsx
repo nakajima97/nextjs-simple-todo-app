83ea75e5b9180ea2bf9964fb6bddc1923d588000
nakajima97 <kituneudon97@gmail.com>
2024-07-16 22:01:51 +0900

    #2 biomeの実行

--- src/pages/task.tsx
+++ src/pages/task.tsx
@@ -1,47 +1,46 @@
-import React from 'react';
-import {
-  Grid,
-  Typography,
-  List,
-  ListItem,
-  ListItemText,
-  Checkbox,
-  TextField,
-  Button,
-	IconButton,
-} from '@mui/material';
 import DoneIcon from '@mui/icons-material/Done';
+import {
+	Button,
+	Checkbox,
+	Grid,
+	IconButton,
+	List,
+	ListItem,
+	ListItemText,
+	TextField,
+	Typography,
+} from '@mui/material';
+import React from 'react';
 
 const tasks = [
-  { id: 1, title: '買い物に行く', description: 'スーパーに買い物に行く' },
-  { id: 2, title: 'レポートを書く', description: 'レポートを提出する' },
-  { id: 3, title: '会議の準備', description: '会議資料を作成する' },
+	{ id: 1, title: '買い物に行く', description: 'スーパーに買い物に行く' },
+	{ id: 2, title: 'レポートを書く', description: 'レポートを提出する' },
+	{ id: 3, title: '会議の準備', description: '会議資料を作成する' },
 ];
 
 const TaskList = () => {
-  return (
-    <Grid container spacing={2} p={2}>
-      <Grid item xs={12}>
-        <Typography variant="h4">タスク一覧</Typography>
-      </Grid>
-      <Grid item xs={12}>
-        <List>
-          {tasks.map((task) => (
-            <ListItem key={task.id} sx={{display: 'flex', gap: 2}}>
-							<IconButton aria-label='finish task'><DoneIcon /></IconButton>
-              <ListItemText
-                primary={task.title}
-                secondary={task.description}
-              />
-            </ListItem>
-          ))}
-        </List>
-      </Grid>
-      <Grid item xs={12}>
-        <Button variant="contained">タスクを追加する</Button>
-      </Grid>
-    </Grid>
-  );
+	return (
+		<Grid container spacing={2} p={2}>
+			<Grid item xs={12}>
+				<Typography variant="h4">タスク一覧</Typography>
+			</Grid>
+			<Grid item xs={12}>
+				<List>
+					{tasks.map((task) => (
+						<ListItem key={task.id} sx={{ display: 'flex', gap: 2 }}>
+							<IconButton aria-label="finish task">
+								<DoneIcon />
+							</IconButton>
+							<ListItemText primary={task.title} secondary={task.description} />
+						</ListItem>
+					))}
+				</List>
+			</Grid>
+			<Grid item xs={12}>
+				<Button variant="contained">タスクを追加する</Button>
+			</Grid>
+		</Grid>
+	);
 };
 
 export default TaskList;
