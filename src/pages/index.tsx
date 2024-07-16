e14a591b6d89ed9c6266ea75e07c9b0fb1fcb8eb
Nakajima <kituneudon97@gmail.com>
2024-07-16 22:04:33 +0900

    静的バージョンの作成 (#4)
    * #2 MUIのインストール
    
    * #2 ログインページの静的バージョン作成
    
    * #2 画面遷移を実装
    
    * #2 タスク一覧画面の静的バージョンを作成
    
    * #2 レイアウトの微調整
    
    * #2 biomeの実行

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,30 @@
+import { Box, Button, Container, CssBaseline } from '@mui/material';
+import { useRouter } from 'next/router';
+
 export default function Home() {
-	return <main>Hello World!!</main>;
+	const router = useRouter();
+
+	const handleLogin = () => {
+		router.push('/task');
+	};
+
+	return (
+		<Container component="main" maxWidth="xs">
+			<CssBaseline />
+			<Box
+				sx={{
+					marginTop: 8,
+					display: 'flex',
+					flexDirection: 'column',
+					alignItems: 'center',
+				}}
+			>
+				<Box sx={{ mt: 1 }}>
+					<Button onClick={handleLogin} variant="contained">
+						ログイン
+					</Button>
+				</Box>
+			</Box>
+		</Container>
+	);
 }
