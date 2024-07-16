fed2d397f0e7fc9564d01750b2df550e39dba738
nakajima97 <kituneudon97@gmail.com>
2024-07-16 21:35:55 +0900

    #2 MUIのインストール

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,10 @@
+import { AppCacheProvider } from '@mui/material-nextjs/v13-pagesRouter';
 import type { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
-	return <Component {...pageProps} />;
+	return (
+		<AppCacheProvider>
+			<Component {...pageProps} />
+		</AppCacheProvider>
+	);
 }
