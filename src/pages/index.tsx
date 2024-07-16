76f79ab430a75a28daba172247556851f24b8eae
nakajima97 <kituneudon97@gmail.com>
2024-07-16 21:40:18 +0900

    #2 ログインページの静的バージョン作成

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,20 @@
+import { Box, Container, CssBaseline, Button } from "@mui/material";
+
 export default function Home() {
-	return <main>Hello World!!</main>;
+	return <Container component="main" maxWidth="xs">
+	<CssBaseline />
+	<Box
+		sx={{
+			marginTop: 8,
+			display: 'flex',
+			flexDirection: 'column',
+			alignItems: 'center',
+		}}
+	>
+
+		<Box sx={{ mt: 1 }}>
+			<Button variant="contained">ログイン</Button>
+		</Box>
+	</Box>
+</Container>;
 }
