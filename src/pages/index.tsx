3372b45ef2ffc506c59673c7642efda4864ff19d
nakajima97 <kituneudon97@gmail.com>
2024-07-25 08:42:30 +0900

    #9 静的なバージョンを作成

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,14 @@
-import { Box, Button, Container, CssBaseline } from '@mui/material';
+import {
+	Box,
+	Button,
+	Checkbox,
+	Container,
+	CssBaseline,
+	FormControlLabel,
+	Grid,
+	TextField,
+	Typography,
+} from '@mui/material';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 
@@ -11,7 +21,6 @@ export default function Home() {
 
 	return (
 		<Container component="main" maxWidth="xs">
-			<CssBaseline />
 			<Box
 				sx={{
 					marginTop: 8,
@@ -20,13 +29,51 @@ export default function Home() {
 					alignItems: 'center',
 				}}
 			>
-				<Box sx={{ mt: 1 }}>
-					<Button onClick={handleLogin} variant="contained">
-						ログイン
+				<Typography component="h1" variant="h5">
+					Sign in
+				</Typography>
+				<Box>
+					<TextField
+						margin="normal"
+						required
+						fullWidth
+						id="email"
+						label="Email Address"
+						name="email"
+						autoComplete="email"
+						autoFocus
+					/>
+					<TextField
+						margin="normal"
+						required
+						fullWidth
+						name="password"
+						label="Password"
+						type="password"
+						id="password"
+						autoComplete="current-password"
+					/>
+					<FormControlLabel
+						control={<Checkbox value="remember" color="primary" />}
+						label="Remember me"
+					/>
+					<Button
+						type="button"
+						fullWidth
+						variant="contained"
+						sx={{ mt: 3, mb: 2 }}
+						onClick={handleLogin}
+					>
+						Sign In
 					</Button>
-				</Box>
-				<Box sx={{ mt: 2 }}>
-					<Link href="/register">または新規登録</Link>
+					<Grid container>
+						<Grid item xs>
+							<Link href="#">パスワードを忘れた場合</Link>
+						</Grid>
+						<Grid item>
+							<Link href="/register">新規登録はこちら</Link>
+						</Grid>
+					</Grid>
 				</Box>
 			</Box>
 		</Container>
