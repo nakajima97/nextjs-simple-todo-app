116c63f8cc7e5b3b770c7b430cde139dc4713e05
nakajima97 <kituneudon97@gmail.com>
2024-07-28 11:19:16 +0900

    #16 CssBaselineは別の場所のほうが良いので移動

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,13 @@
 import { FirebaseProvider } from '@/contexts/firebaseProvider';
 import { AppCacheProvider } from '@mui/material-nextjs/v13-pagesRouter';
+import { CssBaseline } from '@mui/material'; // Import CSSBaseline from the appropriate package
 import type { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
 	return (
 		<AppCacheProvider>
 			<FirebaseProvider>
+				<CssBaseline />
 				<Component {...pageProps} />
 			</FirebaseProvider>
 		</AppCacheProvider>
--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -5,7 +5,6 @@ import {
 	Box,
 	Button,
 	Container,
-	CssBaseline,
 	Grid,
 	TextField,
 	Typography,
@@ -53,7 +52,6 @@ export default function SignUp() {
 
 	return (
 		<Container component="main" maxWidth="xs">
-			<CssBaseline />
 			<Box
 				sx={{
 					marginTop: 8,
